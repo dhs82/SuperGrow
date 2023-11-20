3b99fec0444dcbe6ceb5220a86e692528ad57f42
unknown <0214gxqs@gmail.com>
2023-11-20 18:21:51 +0900

    calendar-add(#17)

--- app/src/main/java/com/example/canlender_openpro/Fragment/FragStar.java
+++ app/src/main/java/com/example/canlender_openpro/Fragment/FragStar.java
@@ -5,17 +5,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.canlender_openpro.R;
 
-
 public class FragStar extends Fragment {
-    private View view;
 
+    private View view;
     private String TAG = "프래그먼트";
 
     @Nullable
@@ -24,27 +26,7 @@ public class FragStar extends Fragment {
         Log.i(TAG, "onCreateView");
         view = inflater.inflate(R.layout.frag_star, container, false);
 
-
-        return view;
-    }
-
-}
-//달력
-import android.os.Bundle;
-        import android.widget.CalendarView;
-        import android.widget.Toast;
-
-        import androidx.annotation.NonNull;
-        import androidx.appcompat.app.AppCompatActivity;
-
-public class FragStar extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.frag_star);
-
-        CalendarView calendarView = findViewById(R.id.calendarView);
+        CalendarView calendarView = view.findViewById(R.id.calendarView);
 
         // 달력에서 날짜를 선택했을 때 이벤트 처리
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -52,8 +34,10 @@ public class FragStar extends AppCompatActivity {
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 // 선택된 날짜에 대한 처리를 여기에 추가
                 String selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
-                Toast.makeText(FragStar.this, "Selected Date: " + selectedDate, Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireContext(), "Selected Date: " + selectedDate, Toast.LENGTH_SHORT).show();
             }
         });
+
+        return view;
     }
 }
